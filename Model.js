4e979d6186c661c63ba87e5b0dd2feaa39708e03
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2016-11-23 23:15:37 +0100

    some code cleanup

--- Model.js
+++ Model.js
@@ -3,9 +3,9 @@
 function RecorderModel() {
     var startDate = new Date();
     var archive = [];
-    var recorded = [];
-    var activeStroke = null;
     var activeRecord = null;
+    var activeStrokes = [];
+    var activeStroke = null;
 
     function createRecord(text) {
         activeRecord = {
@@ -13,30 +13,34 @@ function RecorderModel() {
             description: text,
             client: "Qt Recorder"
         };
-        recorded = activeRecord.strokes
+        startDate = new Date();
+        activeStrokes = activeRecord.strokes;
     }
 
     function theActiveStroke() {
         if (null === activeStroke) {
             activeStroke = [];
-            recorded.push(activeStroke);
+            activeStrokes.push(activeStroke);
         }
         return activeStroke;
     }
 
     function finishRecord() {
         if (null === activeRecord) return;
-        if (0 === recorded.length || 0 === recorded[0].length) return;
-        archive.push(activeRecord);
+        if (0 !== activeStrokes.length && 0 !== activeStrokes[0].length) {
+            archive.push(activeRecord);
+        }
         activeRecord = null;
-        recorded = null;
+        activeStrokes = [];
         activeStroke = null;
     }
 
-
     return {
         sendArchive: function () {
-           // TODO
+            if (0 === archive.length) return;
+            var load = JSON.stringify(archive);
+            // TODO
+            archive = [];
         },
         done: finishRecord,
         resetInput: function (text) {
@@ -44,50 +48,29 @@ function RecorderModel() {
             createRecord(text);
         },
         recordPoint: function (point) {
-           var date = new Date();
-           var item = {
-               t: date - startDate,
-               x: point.x,
-               y: point.y,
-               p: point.pressure
-           };
-           theActiveStroke().push(item);
-           //console.log(JSON.stringify(recorded));
+            var date = new Date();
+            var item = {
+                t: date - startDate,
+                x: point.x,
+                y: point.y,
+                p: point.pressure
+            };
+            theActiveStroke().push(item);
         },
         recordStrokeDone: function () {
-           activeStroke = null;
+            activeStroke = null;
         },
         paint: function (ctx, canvas) {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            if (null === recorded) return;
-            recorded.forEach(function(stroke) {
-               if (stroke.length === 0)
-                   return;
-               ctx.beginPath();
-               ctx.moveTo(stroke[0].x, stroke[0].y);
-               for (var i = 1; i < stroke.length; ++i) {
-                   ctx.lineTo(stroke[i].x, stroke[i].y);
-               }
-               ctx.stroke();
+            activeStrokes.forEach(function(stroke) {
+                if (stroke.length === 0) return;
+                ctx.beginPath();
+                ctx.moveTo(stroke[0].x, stroke[0].y);
+                for (var i = 1; i < stroke.length; ++i) {
+                    ctx.lineTo(stroke[i].x, stroke[i].y);
+                }
+                ctx.stroke();
             });
         }
     };
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
