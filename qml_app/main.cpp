ffb0c185ef01358d1dfdc9a2cb06e5430baff9da
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2018-02-25 13:40:07 +0100

    report SSL Versions

--- qml_app/main.cpp
+++ qml_app/main.cpp
@@ -3,10 +3,16 @@
 #include <QQmlContext>
 #include <QSysInfo>
 
+#include <QDebug>
+#include <QSslSocket>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
+
+    qInfo() << "sslLibraryBuildVersion" << QSslSocket::sslLibraryBuildVersionString();
+    qInfo() << "sslLibraryVersion" << QSslSocket::sslLibraryVersionString();
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("cppHostName", QSysInfo::machineHostName());
